886615effb6f878509ea285a7029fea9a2743492
XDavider <daviddedora@gmail.com>
2025-04-13 13:24:27 +0200

    feat: configuración del login y creación de superusuario
    - Se configuró el formulario de login en la vista `login_view`.
    - Se añadieron mensajes de error para la autenticación fallida.
    - Se verificó y ajustó la configuración de `INSTALLED_APPS` en `settings.py` para incluir `django.contrib.admin`.
    - Se realizaron las migraciones necesarias para asegurarse de que las tablas de la base de datos estén actualizadas.
    - Se configuró correctamente la URL para el panel de administración de Django.

--- Surfitos/urls.py
+++ Surfitos/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from SurfApp import views
 
 urlpatterns = [
-#  path('admin/', admin.site.urls),
-    path('', views.login )
+    path('admin/', admin.site.urls),
+    path('', views.login_view, name='login' ),
+    path('home/', views.home_view, name='home' ),
 ]
